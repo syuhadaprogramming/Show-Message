c400a095d71487b84a5fd9bc31092a4322732902
syuhadaprogramming <syuhadaprogramming@gmail.com>
2020-09-30 04:26:26 +0700

    finis script show massege project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('object');
+// get element
+const bShowMessage = document.querySelector(".btn-primary");
+const bHidenMessage = document.querySelector(".btn-secondary");
+const bodyCard = document.querySelector(".body-card");
+
+// do if button show message click
+bShowMessage.addEventListener("click", () => {
+  bodyCard.classList.toggle("show-message");
+  bShowMessage.classList.toggle("hide-message");
+  bHidenMessage.classList.toggle("show-message");
+});
+
+// do if button hide message click
+bHidenMessage.addEventListener("click", () => {
+  bHidenMessage.classList.toggle("show-message");
+  bShowMessage.classList.toggle("hide-message");
+  bodyCard.classList.toggle("show-message");
+});
